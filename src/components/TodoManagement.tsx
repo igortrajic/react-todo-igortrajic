6441897c20c63592341b63bd7ebb219ab2c58091
igortrajic <iggetrajic@gmail.com>
2026-01-06 11:32:36 +0100

    feat: added delete all todos funcion closes #12

--- src/components/TodoManagement.tsx
+++ src/components/TodoManagement.tsx
@@ -3,9 +3,12 @@ import { useAppStore } from '../useStore';
 export default function TodoManagement() {
   const setSortType = useAppStore((state) => state.setSortType);
   const setFilterType = useAppStore((state) => state.setFilterType);
+  const removeAllTodos = useAppStore((state) => state.removeAllTodos);
   return (
     <div className="todo-container">
-      <button className="buttons red">Delete all</button>
+      <button className="buttons red" onClick={removeAllTodos}>
+        Delete all
+      </button>
       <select className="buttons" onChange={(e) => setSortType(e.target.value)}>
         <option value="name">By Name</option>
         <option value="date">By Date</option>
